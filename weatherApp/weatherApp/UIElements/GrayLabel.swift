b2ca62528d9ee2d755b73600148d1dae423d0076
Max Vakhbe <maxim.vakhbe@gmail.com>
2021-08-19 17:53:56 +0300

    + Констрейнты для view с влажностью и скоростью ветра
    упустил в назначенном коммите

--- weatherApp/weatherApp/UIElements/GrayLabel.swift
+++ weatherApp/weatherApp/UIElements/GrayLabel.swift
@@ -5,11 +5,12 @@
 //  Created by Mix174 on 28.07.2021.
 //
 import UIKit
+import SnapKit
 
 final class GrayLabel: UIView {
     // Аутлеты
-    @IBOutlet weak var type: UILabel!
-    @IBOutlet weak var value: UILabel!
+    @IBOutlet private weak var type: UILabel!
+    @IBOutlet private weak var value: UILabel!
     
     // Автонастройка лейбла
     override func awakeFromNib() {
@@ -22,6 +23,7 @@ final class GrayLabel: UIView {
         backgroundColor = UIColor.alebaster065
         layer.cornerRadius = 10
         layer.masksToBounds = true
+        
     }
     
     // Выбор показателя для лейбла
@@ -34,5 +36,26 @@ final class GrayLabel: UIView {
     func labelSetup(type: ParameterType, value: String) {
         self.type.text = type.rawValue
         self.value.text = value
+    }
+    
+    // Установка констрейтов
+    func setConstraints() {
+        let superViewSnp = self.snp
+        self.translatesAutoresizingMaskIntoConstraints = false
+        type.snp.makeConstraints { make in
+            make.top.equalTo(superViewSnp.top).inset(10)
+            make.leading.equalTo(superViewSnp.leading).inset(15)
+            make.trailing.equalTo(superViewSnp.trailing).inset(15)
+        }
+        value.snp.makeConstraints { make in
+            make.top.equalTo(type.snp.bottom).offset(5)
+            make.bottom.equalTo(superViewSnp.bottom).inset(10)
+            make.leading.equalTo(type.snp.leading)
+            make.trailing.equalTo(type.snp.trailing)
+        }
+    }
+    func removeInnerConstraints() {
+        type.snp.removeConstraints()
+        value.snp.removeConstraints()
     }
 }
